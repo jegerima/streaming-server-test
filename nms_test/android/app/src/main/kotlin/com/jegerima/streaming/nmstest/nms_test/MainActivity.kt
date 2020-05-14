fcde1ce7461b6fc0f2530c5241b9cf0e2d655cac
jegerima <jegerima@gmail.com>
2020-05-14 00:56:52 -0500

    app in order to consume the streaming in a webview

---
+++ nms_test/android/app/src/main/kotlin/com/jegerima/streaming/nmstest/nms_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jegerima.streaming.nmstest.nms_test
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
